998c2f1be9a9dc050d26be2ecf197787693b217b
Ruslan Gaziev <grinrus@yandex.ru>
2022-03-24 13:02:53 +0300

    ПР4 Первая итерация 1.0

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+const popup = document.querySelector('.popup');
+const popupOpen = document.querySelector('.profile__edit-button');
+const popupClose = popup.querySelector('.popup__close');
+
+let formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.popup__input-user');
+const jobInput = document.querySelector('.popup__input-occupation');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+function saveProfile() {
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  togglePopup();
+}
+
+popupOpen.addEventListener('click', togglePopup);
+popupClose.addEventListener('click', togglePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
+
+
+
