0f0d233fb406c41ce96cafc98d119585e8a8f663
Ruslan Gaziev <grinrus@yandex.ru>
2022-03-27 01:17:58 +0300

    ПР4 Вторая итерация 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,26 @@ const popup = document.querySelector('.popup');
 const popupOpen = document.querySelector('.profile__edit-button');
 const popupClose = popup.querySelector('.popup__close');
 
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input-user');
 const jobInput = document.querySelector('.popup__input-occupation');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
-}
 
-function saveProfile() {
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
+
+
+function openPopup() {
+  popup.classList.add('popup_opened');  //открываем попап
+  nameInput.value = profileTitle.textContent;  //подставляем данные из профиля имя в форму
+  jobInput.value = profileSubtitle.textContent;  //подставляем данные из профиля работа в форму
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');  //закрытие попапа через кнопку закрыть
 }
 
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt) {  //закрытие попапа с сохранением и отправкой данных из формы
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
