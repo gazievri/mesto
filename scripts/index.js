ebcaf61140b1a60cdab52e79d86a80cb38bf7783
Ruslan Gaziev <grinrus@yandex.ru>
2022-03-29 01:50:14 +0300

    ПР5 6 карточек с помощью js

--- scripts/index.js
+++ scripts/index.js
@@ -30,6 +30,47 @@ popupClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
+// Добавляем в автозагрузку шесть карточек из коробки
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const elementList = document.querySelector('.elements'); //Объявляю переменную контейнер для новых карточек
+
+function renderInitialCards (item) {
+  const elementTemplate = document.querySelector('.element-template').content.firstElementChild.cloneNode(true); //Нахожу макет карточки и клонирую его
+  elementTemplate.querySelector('.element__title-text').textContent = item.name; //Вставляю в макет название карточки
+  elementTemplate.querySelector('.element__pic').src = item.link; //Вставляю в элемент картинка ссылку на изображение
+  elementTemplate.querySelector('.element__pic').alt = item.name; //Вставляю в alt текст из названия карточки
+  elementList.append(elementTemplate); //Вставляю заполненный макет карточки в контейнер для новых карточек
+}
+
+  initialCards.map(renderInitialCards); //ЗАпускаем функцию renderInitialCards для каждого элемента массива initialCards методом map.
 
 
 
