91970019d01d40ef61c928f9dec897867f94e3dc
Ruslan Gaziev <grinrus@yandex.ru>
2022-03-29 20:59:20 +0300

    ПР5 Функция добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,7 @@ function openPopup() {
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');  //закрытие попапа через кнопку закрыть
+  popup.classList.remove('popup_opened');  //закрытие попапа
 }
 
 function formSubmitHandler (evt) {  //закрытие попапа с сохранением и отправкой данных из формы
@@ -63,21 +63,54 @@ const initialCards = [
 const elementList = document.querySelector('.elements'); //Объявляю переменную контейнер для новых карточек
 
 function renderInitialCards (item) {
-  const elementTemplate = document.querySelector('.element-template').content.firstElementChild.cloneNode(true); //Нахожу макет карточки и клонирую его
+  const elementTemplate = document.querySelector('#element-template').content.firstElementChild.cloneNode(true); //Нахожу макет карточки и клонирую его
   elementTemplate.querySelector('.element__title-text').textContent = item.name; //Вставляю в макет название карточки
   elementTemplate.querySelector('.element__pic').src = item.link; //Вставляю в элемент картинка ссылку на изображение
   elementTemplate.querySelector('.element__pic').alt = item.name; //Вставляю в alt текст из названия карточки
+  elementTemplate.querySelector('.element__bin').addEventListener('click', removeCard); //Добавляю вызов функции removeCard по событию нажатия на кнопку корзина
   elementList.append(elementTemplate); //Вставляю заполненный макет карточки в контейнер для новых карточек
 }
 
-  initialCards.map(renderInitialCards); //ЗАпускаем функцию renderInitialCards для каждого элемента массива initialCards методом map.
+//Создаю функцию удаления карточки
+function removeCard(event) {
+  const card = event.currentTarget.closest('.element'); //Ищем карточку ближайщую к событию (нажатие на кнопку)
+  card.remove(); //Удаление карточки
+}
+
+initialCards.map(renderInitialCards); //Запускаем функцию renderInitialCards для каждого элемента массива initialCards методом map.
 
 
+//Открываем и закрываем новый попап для добавления новой карточки
+const popupPlace = document.querySelector('.place');
+const popupPlaceOpen = document.querySelector('.profile__button');
+const popupPlaceClose = popupPlace.querySelector('.popup__close');
+
+function openPopupPlace() {
+  popupPlace.classList.add('popup_opened');  //открываем попап
+}
+
+function closePopupPlace() {
+  popupPlace.classList.remove('popup_opened');  //закрытие попапа
+}
+
+popupPlaceOpen.addEventListener('click', openPopupPlace);
+popupPlaceClose.addEventListener('click', closePopupPlace);
 
 
+//Добавляем новую карточку
 
+let formNewPlace = document.querySelector('.popup__form-place');
+const placeInput = document.querySelector('.popup__input_field_place');
+const linkInput = document.querySelector('.popup__input_field_link');
 
+function addNewCard() {
+  const newCard = document.querySelector('#element-template').content.firstElementChild.cloneNode(true); //Нахожу макет карточки и клонирую его
+  newCard.querySelector('.element__title-text').textContent = placeInput.value; //Вставляю новое название места
+  newCard.querySelector('.element__pic').src = linkInput.value; //Вставляю ссылку на новую картинку
+  newCard.querySelector('.element__pic').alt = placeInput.value; //Вставляю новый alt к картинке
+  newCard.querySelector('.element__bin').addEventListener('click', removeCard); //Вставляю мониторинг нажатия кнопки удаления и запуск ранее созданной функции удаления
+  elementList .prepend(newCard); //Вставляю новую карточку в начало списка карточек
+  closePopupPlace();
+}
 
-
-
-
+formNewPlace.addEventListener('submit', addNewCard);
