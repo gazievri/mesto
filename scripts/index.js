f01f75218b25f6e0ea5cb4e76f59208f50973f23
Ruslan Gaziev <grinrus@yandex.ru>
2022-03-31 00:14:27 +0300

    ПР5 Добавлена функция лайк

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 const popup = document.querySelector('.popup');
 const popupOpen = document.querySelector('.profile__edit-button');
 const popupClose = popup.querySelector('.popup__close');
@@ -30,7 +31,7 @@ popupClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
-// Добавляем в автозагрузку шесть карточек из коробки
+// Добавляем в автозагрузку шесть карточек из массива (карточки по умолчанию)
 
 const initialCards = [
   {
@@ -64,11 +65,20 @@ const elementList = document.querySelector('.elements'); //Объявляю пе
 
 function renderInitialCards (item) {
   const elementTemplate = document.querySelector('#element-template').content.firstElementChild.cloneNode(true); //Нахожу макет карточки и клонирую его
+  const elementImage = elementTemplate.querySelector('.element__pic');
   elementTemplate.querySelector('.element__title-text').textContent = item.name; //Вставляю в макет название карточки
-  elementTemplate.querySelector('.element__pic').src = item.link; //Вставляю в элемент картинка ссылку на изображение
-  elementTemplate.querySelector('.element__pic').alt = item.name; //Вставляю в alt текст из названия карточки
+  elementImage.src = item.link; //Вставляю в элемент картинка ссылку на изображение
+  elementImage.alt = item.name; //Вставляю в alt текст из названия карточки
+
   elementTemplate.querySelector('.element__bin').addEventListener('click', removeCard); //Добавляю вызов функции removeCard по событию нажатия на кнопку корзина
+
+  elementTemplate.querySelector('.element__title-like').addEventListener('click', getLike); //Добавляю вызов функции openImagePopup при нажатие на картинку карточки
+
+  elementTemplate.querySelector('.element__pic').addEventListener('click', openImagePopup);
+
   elementList.append(elementTemplate); //Вставляю заполненный макет карточки в контейнер для новых карточек
+
+  return elementTemplate;
 }
 
 //Создаю функцию удаления карточки
@@ -77,7 +87,31 @@ function removeCard(event) {
   card.remove(); //Удаление карточки
 }
 
-initialCards.map(renderInitialCards); //Запускаем функцию renderInitialCards для каждого элемента массива initialCards методом map.
+//Создаем функцию поставить лайк
+function getLike(event) {
+  const like = event.currentTarget.closest('.element__title-like');
+  like.classList.toggle('element__title-like_active');
+}
+
+//Создаем функцию открытия попапа картинки карточки
+
+function openImagePopup(elementImage) {
+  const imagePopup = document.querySelector('.image-popup');
+  imagePopup.querySelector('.image-popup__img').src = elementImage.src;
+
+  imagePopup.classList.add('image-popup__opened');
+}
+
+//Создаем функцию закрытия попапа картинки карточки
+const imagePopup = document.querySelector('.image-popup');
+
+function closeImagePopup(event) {
+  const imagePopup = document.querySelector('.image-popup');
+  imagePopup.classList.remove('image-popup__opened');
+
+}
+
+imagePopup.querySelector('.image-popup__close').addEventListener('click', closeImagePopup);//Добавляем мониторинг кнопки закрытия попапа картинки карточки
 
 
 //Открываем и закрываем новый попап для добавления новой карточки
@@ -96,7 +130,6 @@ function closePopupPlace() {
 popupPlaceOpen.addEventListener('click', openPopupPlace);
 popupPlaceClose.addEventListener('click', closePopupPlace);
 
-
 //Добавляем новую карточку
 
 let formNewPlace = document.querySelector('.popup__form-place');
@@ -109,8 +142,12 @@ function addNewCard() {
   newCard.querySelector('.element__pic').src = linkInput.value; //Вставляю ссылку на новую картинку
   newCard.querySelector('.element__pic').alt = placeInput.value; //Вставляю новый alt к картинке
   newCard.querySelector('.element__bin').addEventListener('click', removeCard); //Вставляю мониторинг нажатия кнопки удаления и запуск ранее созданной функции удаления
+  newCard.querySelector('.element__title-like').addEventListener('click', getLike); //Вставляю мониторинг нажатия кнопки лайк и запуск ранее созданной функции поставить лайк
+  newCard.querySelector('.element__pic').addEventListener('click', openImagePopup); //Вставляю мониторинг нажатия на картинку карточки для октрытия попапа картинки
   elementList .prepend(newCard); //Вставляю новую карточку в начало списка карточек
   closePopupPlace();
 }
 
 formNewPlace.addEventListener('submit', addNewCard);
+
+initialCards.map(renderInitialCards); //Запускаем функцию renderInitialCards для каждого элемента массива initialCards методом map.
